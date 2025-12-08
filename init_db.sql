a1df531e7e7f8433c5761187e8885b71e251986c
Koloina01 <kolomaherison@gmail.com>
2025-12-08 15:27:43 +0300

    feat: addingthe response for question 1

---
+++ init_db.sql
@@ -0,0 +1,6 @@
+--1/ Create database,give permission to user
+CREATE DATABASE product_management_db;
+CREATE USER product_manager_user WITH PASSWORD '123456';
+GRANT CONNECT ON DATABASE product_management_db TO product_manager_user;
+GRANT CREATE ON SCHEMA public TO product_manager_user;
+GRANT SELECT,INSERT,UPDATE,DELETE ON ALL TABLES IN SCHEMA public TO product_manager_user;
